b0365cfa23e2b91a2ced058bf52d3db21855b180
Sandra Costa <sanralisistrata@gmail.com>
2023-08-22 00:43:44 -0400

    progress....

--- pathway_modules.py
+++ pathway_modules.py
@@ -3,16 +3,42 @@ import pandas as pd
 from scipy.stats import gmean
 
 
-class PathwayMatrix:
-    def __init__(self) -> None:
-        
+# class PathwayMatrix:
+#     def __init__(self, folder_path,ensemble_id):
+#         self.folder_path = folder_path
+#         self.ensemble_id = ensemble_id
 
-# Read the Top 28 pathways excel list
-top_pathways = pd.read_excel("./data_external/data_pathways/pathways2_sorted.xls",header=None)
-# Name the columns
-top_pathways.columns = ["Ensemble","Pathways"]
 
-# Count pathways
-pathways_names = pd.unique(top_pathways.Pathways)
-# Count genes in pathways
-genes_annotated = pd.unique(top_pathways.Ensemble)
+#     def transform(self,df):
+#         self.matrix = []
+
+#         for p in pathways_names:
+
+
+#     def _pathway_df(self):
+#         pathways = pd.read_excel(self.folder_path,header=None)
+#         pathways.columns = ["Ensemble","Pathways"]
+
+
+pathways_names = pd.unique(pathways.Pathways)
+
+genes_annotated = pd.unique(pathways.Ensemble)
+
+# Genes in pathways
+genes_path = set(ensemble_id) & set(genes_annotated)
+
+# Pathways with annotated genes
+pathways_annotated = top_pathways.query('Ensemble in @genes_path')
+
+def Matrix(col=np.arange()):
+
+X = []
+for pathway in pathways_names:
+    # genes evaluated in pathway (variable to operate)
+    g_pathways = df[
+        pathways_annotated[pathways_annotated.Pathways == pathway].Ensemble
+    ]
+    # Ecuation above
+    X.append(np.sum(abs(g_pathways),axis=1)/g_pathways.shape[1])
+
+X = pd.DataFrame(data=X, columns=tissues,index=pathways_names)
