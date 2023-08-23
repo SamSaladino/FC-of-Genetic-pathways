7fc488e2b97168d12d8c3a480d4cefb738950c11
Sandra Costa <sanralisistrata@gmail.com>
2023-08-23 17:49:35 -0400

    Functional non object module

--- pathway_modules.py
+++ pathway_modules.py
@@ -2,126 +2,25 @@ import numpy as np
 import pandas as pd
 
 
-class PathwayAnalyzer:
-    """Pathway expression matrix transformation
-    It takes the gene expression matrix and the pathway file
-    and get the pathway expression matrix
+def pathway_query(path,ensemble_ids):
+    pathways = pd.read_excel(path, header=None)
+    pathways.columns = ['Pathways', 'Ensemble']
 
-    Parameter:
-    ------------
-        path : string
-            An excel file path.
+    genes_annotated = pd.unique(pathways.Ensemble)
+    genes_path = set(genes_annotated) & set(ensemble_ids)
 
-    Attributes:
-    --------------
-        pathways  : pandas DataFrame
-            A two columns pandas DataFrame with corresponding 
-            ensemble ids.
-        pathway_names : pandas DataFrame
-            Non repeated pathway names
-    """
-    def __init__(self,path):
-        self.pathways = pd.read_excel(path,header=None)
-        self.pathways.columns = ['Pathways','Ensemble']
+    return pathways.query('Ensemble in @genes_path')
 
-    def pathway_query(self,ensemble_ids):
-        genes_annotated = pd.unique(self.pathways.Ensemble)
-        genes_path = set(genes_annotated) & set(ensemble_ids)
-        return self.pathways.query('Ensemble in @genes_path')
+def pathway_expression(expression_matrix,pathway_names, pathway_data):
+    X = []
+    for p in pathway_names:
+        queried_pathways = pathway_data[pathway_data.Pathways == p]
+        g_pathways = queried_pathways.Ensemble
+        matrix_values = np.sum(abs(expression_matrix[g_pathways]), axis=1) / g_pathways.shape[0]
+        X.append(matrix_values)
+    
+    return pd.DataFrame(data=X, index=pathway_names)
 
-    def calculate_matrix(self, data_frame, ensemble_ids, col=False, col_name=None):
-        self.pathway_names = pd.unique(self.pathway_query.Pathways)
-        self.pathways_query(ensemble_ids)
-        X = []
-        for p in self.pathway_names:
-            g_pathways = self.pathways_query(self.pathways[self.pathways.Pathways == p].Ensemble)
-            # Modify the following calculation as needed
-            matrix_values = np.sum(abs(data_frame[g_pathways]), axis=1) / g_pathways.shape[1]
-            X.append(matrix_values)
-        
-        if col:
-            return pd.DataFrame(data=X, columns=col_name, index=self.pathway_names)
-        else:
-            return pd.DataFrame(data=X, index=self.pathway_names)
-        
 
 if __name__ == '__main__':
-    pass
-
-
-# # # class PathwayMatrix:
-# # #     def __init__(self, folder_path,ensemble_id):
-# # #         self.folder_path = folder_path
-# # #         self.ensemble_id = ensemble_id
-
-
-# # #     def transform(self,df):
-# # #         self.matrix = []
-
-# # #         for p in pathways_names:
-
-
-# # #     def _pathway_df(self):
-# # #         pathways = pd.read_excel(self.folder_path,header=None)
-# # #         pathways.columns = ["Ensemble","Pathways"]
-
-
-# # def pathways_query(path=str, ensemble_id=list):
-# #     pathways = pd.read_excel(path, header=None)
-# #     pathways.columns = ["Ensemble","Pathways"]
-# #     genes_annotated = pd.unique(pathways.Ensemble)
-# #     genes_path = set(ensemble_id) & set(genes_annotated)
-    
-# #     return pathways.query('Ensemble in @genes_path')
-
-
-
-# # def matrix(df,col=False, col_name=None):
-# #     pathways_names = pd.unique(pathways_query.Pathways)
-# #     X = []
-# #     for p in pathways_names:
-# #         g_pathways = df[
-# #             pathways_query[
-# #                 pathways_query.Pathways == p
-# #                 ].Ensemble
-# #             ]
-# #         X.append(np.sum(abs(g_pathways),axis=1)/g_pathways.shape[1])
-    
-# #     if col:
-# #         return pd.DataFrame(data=X, columns=col_name,index=pathways_names)
-# #     else:
-# #         return pd.DataFrame(data=X,index=pathways_names)
-# import numpy as np
-# import pandas as pd
-
-# class PathwayAnalyzer:
-#     def __init__(self, path):
-#         self.pathways = pd.read_excel(path, header=None)
-#         self.pathways.columns = ["Ensemble", "Pathways"]
-#         self.pathway_names = pd.unique(self.pathways.Pathways)
-        
-#     def pathways_query(self, ensemble_ids):
-#         genes_annotated = pd.unique(self.pathways.Ensemble)
-#         genes_path = set(ensemble_ids) & set(genes_annotated)
-#         return self.pathways.query('Ensemble in @genes_path')
-    
-#     def calculate_matrix(self, data_frame, ensemble_ids, col=False, col_name=None):
-#         self.pathways_query(ensemble_ids)
-#         X = []
-#         for p in self.pathway_names:
-#             g_pathways = self.pathways_query(self.pathways[self.pathways.Pathways == p].Ensemble)
-#             # Modify the following calculation as needed
-#             matrix_values = np.sum(abs(data_frame[g_pathways]), axis=1) / g_pathways.shape[1]
-#             X.append(matrix_values)
-        
-#         if col:
-#             return pd.DataFrame(data=X, columns=col_name, index=self.pathway_names)
-#         else:
-#             return pd.DataFrame(data=X, index=self.pathway_names)
-
-# Usage
-# pathway_analyzer = PathwayAnalyzer("path_to_excel_file.xlsx")
-# ensemble_ids = [list_of_ensemble_ids]
-# data_frame = pd.DataFrame(...)  # Provide your data frame here
-# result_matrix = pathway_analyzer.calculate_matrix(data_frame, col=True, col_name=["Pathway1", "Pathway2", ...])
-# print(result_matrix)
+    print("run")
